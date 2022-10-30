f60a8034a61f3dfb86799e4f9097e2497e047f29
zorromen <zorromen@mail.ru>
2022-10-30 17:06:15 +0300

    V146.1

--- samples/generator2/app/src/main/java/com/example/generator2/scripting/script.kt
+++ samples/generator2/app/src/main/java/com/example/generator2/scripting/script.kt
@@ -1,5 +1,9 @@
 package com.example.generator2.scripting
 
+import androidx.compose.ui.input.key.Key.Companion.F
+import com.example.generator2.Global
+import com.example.generator2.Utils
+
 /*
  * ----------------- Логика -----------------
  * IF R1 = 2
@@ -54,10 +58,7 @@ package com.example.generator2.scripting
  *
  */
 
-
 const val PC_MAX = 128 //Максимальный размер скрипта..При динамическом списке не имеет смысла
-const val CMD_LEN = 24 //Строки
-
 
 //Основной класс скриптовой системы
 class Script {
@@ -65,21 +66,18 @@ class Script {
     //╭─ Генератор ───────────────────────╮
     var scriptName: String = ""    //Имя текущего скрипта
 
-    //pair_operand   pair;
-    //triple_operand triple;
-
     var end = true
     var yield = false
     var pc = 0
 
     var endTime = 0              //Время > которого можно продолжать работу
 
-    var F = FloatArray(10)  //Массив регистров
+    var register = FloatArray(10)  //Массив регистров
 
     var str: String = ""
 
     //Временная строка
-    val list: MutableList<String> = mutableListOf<String>() //Список команд
+    private val list: MutableList<String> = mutableListOf<String>() //Список команд
     var line = 0;                                           //Текучая строка
 //╰─────────────────────────────╯
 
@@ -106,12 +104,12 @@ class Script {
     fun resume() {
         end = false
     }
-    
+
     //Выполнить команду по строке pc
     fun CMD_EXE() {
 
         val comand: String = list[pc]
-        
+
         println("Script: $pc $comand")
 
         //Разобрать строку на список команд
@@ -144,8 +142,13 @@ class Script {
                 end = true
             }
 
-            //"IF" -> ifComand()
-            //"CH1", "CH2", "CR1", "CR2", "AM1", "AM2","FM1","FM2" -> {generatorComand() pc++}
+            "IF" -> ifComand()
+
+            "CH1", "CH2", "CR1", "CR2", "AM1", "AM2", "FM1", "FM2" -> {
+                generatorComand()
+                pc++
+            }
+
             //"MINUS", "PLUS" -> {comandPlusMinus() pc++}
             //"PRINTF" -> { printF() pc++}
             //"GOTO" -> {  pc = comand.toUint(4); } ???????
@@ -160,14 +163,14 @@ class Script {
             "TEXT" -> pc++
 
             "LOAD" -> {
-            //triple = excretionTripleOperand(comand);
-            //if (triple.operand0 == (char*)"LOAD")
-           // {
-            //    int index = triple.operand1.buf[1]-0x30;
-            //    F[index] = triple.operand2.toFloat();
-            //}
-            pc++
-        }
+                //triple = excretionTripleOperand(comand);
+                //if (triple.operand0 == (char*)"LOAD")
+                // {
+                //    int index = triple.operand1.buf[1]-0x30;
+                //    F[index] = triple.operand2.toFloat();
+                //}
+                pc++
+            }
 
             else -> {
                 println("Script:? pc:$pc:$comand")
@@ -179,6 +182,252 @@ class Script {
         }
 
     }
+
+
+    // IF R1 < 5500
+    fun ifComand() {
+
+        val comand: String = list[pc]
+
+        //Разобрать строку на список команд
+        val listCMD = comand.split(" ")
+        if (listCMD.isEmpty()) {
+            println("Script: Error ifComand размер listCMD == 0")
+            return
+        }
+
+        //IF Rxx Первый всегда R регистр
+        val f1value = register[listCMD[1].drop(1).toInt()]
+
+        var f2value = if ((listCMD[3].first() == 'R'))
+            register[listCMD[3].drop(1).toInt()]
+        else
+            listCMD[3].toFloat()
+
+        // имеем f1value и f2value
+        var boolResult = false //Результат сравнения true or false чтобы решить куда дальше
+
+        if ((listCMD[2] == "<") && (f1value < f2value)) boolResult = true
+        if ((listCMD[2] == ">") && (f1value > f2value)) boolResult = true
+        if ((listCMD[2] == "<=") && (f1value <= f2value)) boolResult = true
+        if ((listCMD[2] == ">=") && (f1value >= f2value)) boolResult = true
+        if ((listCMD[2] == "!=") && (f1value != f2value)) boolResult = true
+        if ((listCMD[2] == "==") && (f1value == f2value)) boolResult = true
+
+        if (boolResult) {
+            pc++ //Переход на следующую строку, ибо условие выполнено
+        } else {
+            //Ищем первое ELSE или ENDIF, ибо условие не выполнено
+            var currentPC = pc
+            while (true) {
+                if (list[currentPC] == "ELSE") //+1 к ELSE
+                {
+                    pc = currentPC + 1
+                    break
+                }
+
+                if (list[currentPC] == "ENDIF") {
+                    pc = currentPC
+                    break
+                }
+                currentPC++
+                if (currentPC > (PC_MAX - 1))
+                    break
+            }
+        }
+    }
+
+    fun generatorComand() {
+
+        val comand: String = list[pc]
+
+        //Разобрать строку на список команд
+        val listCMD = comand.split(" ")
+        if (listCMD.isEmpty()) {
+            println("Script: Error generatorComand размер listCMD == 0")
+            return
+        }
+
+        var value = 0.0f
+
+        val chanel = listCMD[0].drop(2).toInt() //Номер канала
+
+        //var onoff = false
+
+        //╭─ CH1 CH2 ─────────────────────────────────────────────────────────────────╮
+        if ((listCMD[0] == "CH1") || (listCMD[0] == "CH2")) {                       //│                                                                       //│
+            val onoff = listCMD[2] == "ON"                                          //│
+            //│
+            if (listCMD[1] == "CR")                                                 //│
+            {                                                                       //│
+                if (chanel == 1)                                                    //│
+                    Global.ch1_EN.value = onoff                                     //│
+                else                                                                //│
+                    Global.ch2_EN.value = onoff                                     //│
+            }                                                                       //│
+            //│
+            if (listCMD[1] == "AM")                                                 //│
+            {                                                                       //│
+                if (chanel == 1)                                                    //│
+                    Global.ch1_AM_EN.value = onoff                                  //│
+                else                                                                //│
+                    Global.ch2_AM_EN.value = onoff                                  //│
+            }                                                                       //│
+            //│
+            if (listCMD[1] == "FM")                                                 //│
+            {                                                                       //│
+                if (chanel == 1)                                                    //│
+                    Global.ch1_FM_EN.value = onoff                                  //│
+                else                                                                //│
+                    Global.ch2_FM_EN.value = onoff                                  //│
+            }                                                                       //│
+            //│
+            return                                                                  //│
+        }                                                                           //│
+        //╰───────────────────────────────────────────────────────────────────────────╯
+
+        //╭─ CR1 CR2 ─────────────────────────────────────────────────────────────────╮
+        if ((listCMD[0] == "CR1") || (listCMD[0] == "CR2"))                         //│
+        {                                                                           //│
+            //CR[1 2] MOD 01_Sine_12b                                               //│
+            if (listCMD[1] == "MOD")                                                //│
+            {                                                                       //│
+                println(listCMD[2])                                                 //│
+                //│
+                if (chanel == 1) {                                                  //│
+                    Global.ch1_Carrier_Filename.value = listCMD[2]                  //│
+                    Utils.Spinner_Send_Buffer("CH0", "CR", listCMD[2])     //│
+                }                                                                   //│
+                else                                                                //│
+                {                                                                   //│
+                    Global.ch2_Carrier_Filename.value = listCMD[2]                  //│
+                    Utils.Spinner_Send_Buffer("CH1", "CR", listCMD[2])    //│
+                }                                                                   //│
+                //│
+            }                                                                       //│
+            //│
+            //CR[1 2] FR 1000.3                                                     //│
+            if (listCMD[1] == "FR")                                                 //│
+            {                                                                       //│
+                value = if (listCMD[2].first() == 'F') {      //│
+                    register[listCMD[2].drop(1).toInt()]                         //│
+                } else  //│
+                    listCMD[2].toFloat()  //│
+
+                if (chanel == 1)
+                    Global.ch1_Carrier_Fr.value = value                               //│
+                else                                                                  //│
+                    Global.ch2_Carrier_Fr.value = value                               //│
+                //│
+            }                                                                         //│
+            //│
+            return                                                                    //│
+        }                                                                             //│
+        //╰─────────────────────────────────────────────────────────────────────────────╯
+
+        //╭─ AM1 AM2 ───────────────────────────────────────────────────────────────────╮
+        if ((listCMD[0] == "AM1") || (listCMD[0] == "AM2"))                           //│
+        {                                                                             //│
+            //SEGGER_RTT_printf(0, "╭─ AM1 AM2 ─╮\n");                                //│
+            //│
+            //AM[1 2] FR 1000.3                                                       //│
+            if (listCMD[1] == "FR")                                                   //│
+            {                                                                         //│
+                value = if (listCMD[2].first() == 'F') {                              //│
+                    register[listCMD[2].drop(1).toInt()]                           //│
+                } else                                                                //│
+                    listCMD[2].toFloat()                                              //│
+                //│
+                if (chanel == 1)                                                      //│
+                    Global.ch1_AM_Fr.value = value                                    //│
+                else                                                                  //│
+                    Global.ch2_AM_Fr.value = value                                    //│
+            }                                                                         //│
+            //│
+            //AM[1 2] MOD 02_HWawe { 1.9ms }                                          //│
+            if (listCMD[1] == "MOD")                                                  //│
+            {                                                                         //│
+                if (chanel == 1) {                                                    //│
+                    Global.ch1_AM_Filename.value = listCMD[2]                         //│
+                    Utils.Spinner_Send_Buffer("CH0", "AM", listCMD[2])       //│
+                }                                                                     //│
+                else                                                                  //│
+                {                                                                     //│
+                    Global.ch2_AM_Filename.value = listCMD[2]                         //│
+                    Utils.Spinner_Send_Buffer("CH1", "AM", listCMD[2])       //│
+                }                                                                     //│
+            }                                                                         //│
+            return                                                                    //│
+        }                                                                             //│
+        //╰─────────────────────────────────────────────────────────────────────────────╯
+        //╭─ FM1 FM2 ───────────────────────────────────────────────────────────────────╮
+        if ((listCMD[0] == "FM1") || (listCMD[0] == "FM2"))                           //│
+        {                                                                             //│
+            //SEGGER_RTT_printf(0, "╭─ FM1 FM2 ─╮\n");                                //│
+            //│
+            //FM[1 2] BASE 1234.6                                                     //│
+            if (listCMD[1] == "BASE")                                                 //│
+            {                                                                         //│
+                value = if (listCMD[2].first() == 'F') {                              //│
+                    register[listCMD[2].drop(1).toInt()]                           //│
+                } else                                                                //│
+                    listCMD[2].toFloat()                                              //│
+
+                if (chanel == 1)                                                      //│
+                    Global.ch1_FM_Base.value = value                                  //│
+                else                                                                  //│
+                    Global.ch2_FM_Base.value = value                                  //│
+            }                                                                         //│
+        }                                                                             //│
+        //│
+        //FM[1 2] DEV  123.8                                                          //│
+        if (listCMD[1] == "DEV")                                                      //│
+        {
+            value = if (listCMD[2].first() == 'F') {
+                register[listCMD[2].drop(1).toInt()]
+            } else
+                listCMD[2].toFloat()
+
+            if (chanel == 1)
+                Global.ch1_FM_Dev.value = value
+            //│
+            else                                                                     //│
+                Global.ch2_FM_Dev.value = value                                      //│
+
+
+        }                                                                       //│
+        //│
+        //FM[1 2] MOD 02_HWawe                                                  //│
+        if (listCMD[1] == "MOD")                                                //│
+        {                                                                       //│
+            if (chanel == 1) {
+                Global.ch1_FM_Filename.value = listCMD[2]
+                Utils.Spinner_Send_Buffer("CH0", "FM", listCMD[2])
+            }                                                                   //│
+            else                                                                //│
+            {                                                                   //│
+                Global.ch2_FM_Filename.value = listCMD[2]
+                Utils.Spinner_Send_Buffer("CH1", "FM", listCMD[2])    //│
+            }                                                                   //│
+        }                                                                       //│
+
+        //FM[1 2] FR   3.5                                                      //│
+        if (listCMD[1] == "FR")                                                 //│
+        {                                                                       //│
+            value = if (listCMD[2].first() == 'F') {
+                register[listCMD[2].drop(1).toInt()]
+            } else
+                listCMD[2].toFloat()
+
+            if (chanel == 1)
+                Global.ch1_FM_Fr.value = value
+            else                                                                 //│
+                Global.ch2_FM_Fr.value = value                                   //│
+        }                                                                        //│
+        return                                                                   //│
+    }                                                                            //│
+    //╰────────────────────────────────────────────────────────────────────────────╯
+}
 
 
 }
