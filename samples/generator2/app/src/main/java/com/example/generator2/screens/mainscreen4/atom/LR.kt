499a75d0b8351213488300d2ebd8cbe5f148b083
zorromen <zorromen@mail.ru>
2022-12-23 11:25:24 +0300

    V224

--- samples/generator2/app/src/main/java/com/example/generator2/screens/mainscreen4/atom/LR.kt
+++ samples/generator2/app/src/main/java/com/example/generator2/screens/mainscreen4/atom/LR.kt
@@ -1,52 +1,67 @@
 package com.example.generator2.screens.mainscreen4.atom
 
+
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.generator2.data.LiveData
 import com.example.generator2.theme.colorDarkBackground
+import com.example.generator2.R
+
+private val m : Modifier = Modifier.fillMaxHeight().aspectRatio(1.5f).clip(RoundedCornerShape(15.dp)).border(1.dp, Color.DarkGray, CircleShape)
+    .background(colorDarkBackground).offset(1.dp, 0.dp)
+
+val fontSize = 20.sp
 
 @Composable
 fun LR() {
-    Row(
-        modifier = Modifier
-            .height(30.dp)
-            .width(70.dp)
-            .clip(RoundedCornerShape(15.dp))
-            //.background(Color.Red)
-        ,verticalAlignment = Alignment.CenterVertically
-    )
+    Row( modifier = Modifier.height(30.dp).clip(RoundedCornerShape(15.dp)) ,verticalAlignment = Alignment.CenterVertically )
     {
-        Text(text = "L",
-            modifier = Modifier
-                .fillMaxHeight()
-                .aspectRatio(1f)
-                .clip(RoundedCornerShape(15.dp))
-                .border(1.dp, Color.DarkGray, CircleShape)
-                .background(Color.Black)
-                .offset(0.dp, 4.dp),
-            textAlign = TextAlign.Center
+        val mono = LiveData.mono.collectAsState()
+        val shuffle = LiveData.shuffle.collectAsState()
+        val enL = LiveData.enL.collectAsState()
+        val enR = LiveData.enR.collectAsState()
+
+        val colorL = if (enL.value) { if (!shuffle.value) Color.White else { if (!mono.value) Color.Red else Color.White } } else Color.DarkGray
+        val colorR = if (enR.value) { if (!shuffle.value) Color.White else { if (!mono.value) Color.Red else Color.White } } else Color.DarkGray
+
+        Text(text = if (!mono.value) { if (!shuffle.value) "L" else "R" } else "L",
+            modifier = Modifier.then(m).clickable { LiveData.enL.value = !LiveData.enL.value },
+            fontSize = fontSize,
+            textAlign = TextAlign.Center, fontWeight = FontWeight.Bold,
+            color = colorL, fontFamily = FontFamily(Font(R.font.jetbrains))
         )
-        Spacer(modifier = Modifier.width(8.dp))
-        Text(text = "R",
-            modifier = Modifier
-                .padding()
-                .fillMaxHeight()
-                .aspectRatio(1f)
-                .clip(RoundedCornerShape(15.dp))
-                .border(1.dp, Color.DarkGray, CircleShape)
-                .background(Color.Black)
-                .offset(1.dp, 4.dp),
-            textAlign = TextAlign.Center
+
+        Spacer(modifier = Modifier.width(4.dp))
+
+        Text(text =  if (!mono.value) { if (!shuffle.value) "R" else "L" } else "R",
+            modifier = Modifier.then(m).clickable { LiveData.enR.value = !LiveData.enR.value },
+            fontSize = fontSize,
+            textAlign = TextAlign.Center, fontWeight = FontWeight.Bold,
+            color = colorR, fontFamily = FontFamily(Font(R.font.jetbrains))
         )
+
+
+
+
+
     }
 }
--- samples/generator2/app/src/main/java/com/example/generator2/screens/mainscreen4/card/cardComander.kt
+++ samples/generator2/app/src/main/java/com/example/generator2/screens/mainscreen4/card/cardComander.kt
@@ -1,41 +1,38 @@
 package com.example.generator2.screens.mainscreen4.card
 
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.*
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.generator2.R
-import com.example.generator2.theme.colorDarkBackground
-import com.example.generator2.theme.colorLightBackground
 import com.example.generator2.data.LiveData
 import com.example.generator2.screens.mainscreen4.atom.LR
 import com.example.generator2.screens.mainscreen4.vm.VMMain4
+import com.example.generator2.theme.colorDarkBackground
+import com.example.generator2.theme.colorLightBackground
 import com.siddroid.holi.colors.MaterialColor
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun CardCommander( vm : VMMain4) {
 
     Card(
-        Modifier
-            .height(40.dp)
-            .fillMaxWidth()
-            .padding(start = 8.dp, end = 8.dp)
-            .horizontalScroll(rememberScrollState()), backgroundColor = colorLightBackground
-    )
+        Modifier.height(40.dp).fillMaxWidth().padding(start = 8.dp, end = 8.dp)
+            .horizontalScroll(rememberScrollState()), backgroundColor = colorLightBackground )
     {
 
         Row(Modifier.fillMaxSize(), Arrangement.Start, Alignment.CenterVertically) {
@@ -50,20 +47,30 @@ fun CardCommander( vm : VMMain4) {
 
             LR()
 
-            val invert = LiveData.invert.collectAsState()
-            IconButton(onClick = { LiveData.invert.value = !LiveData.invert.value })
-            {
-                val color = if (invert.value) MaterialColor.GREEN_500  else colorDarkBackground
-                Icon(painter = painterResource(R.drawable.arrow_up_arrow_down51), contentDescription = null, tint = color)
-            }
-
             val shuffle = LiveData.shuffle.collectAsState()
-            IconButton( onClick = { LiveData.shuffle.value = !LiveData.shuffle.value  })
-            {
-                val color = if (shuffle.value) MaterialColor.GREEN_500  else colorDarkBackground
-                Icon( painter = painterResource(R.drawable.shuffle74), contentDescription = null , tint = color) }
+            val invert = LiveData.invert.collectAsState()
+            val time = 160
 
+            Crossfade(targetState = mono.value, animationSpec = tween(time))
+            {
+                if (!it) {
+                    IconButton( onClick = { LiveData.shuffle.value = !LiveData.shuffle.value  }) {
+                        val color =  if (shuffle.value) MaterialColor.GREEN_500  else colorDarkBackground
+                        Icon( painter = painterResource(R.drawable.shuffle74), contentDescription = null , tint = color)
+                    }
+                }
+                else {
+                    IconButton(onClick = { LiveData.invert.value = !LiveData.invert.value }) {
+                        val color = if (invert.value) MaterialColor.GREEN_500  else colorDarkBackground
+                        Icon(painter = painterResource(R.drawable.arrow_up_arrow_down51), contentDescription = null, tint = color)
+                    }
+                }
+            }
             Spacer(modifier = Modifier.width(8.dp))
+
+            IconButton(onClick = { LiveData.invert.value = !LiveData.invert.value }) {
+                Icon(painter = painterResource(R.drawable.circle_small), contentDescription = null, tint = Color.LightGray)
+            }
 
         }
 
